bbeb28659685d3a254b217e445b9fb79bdb1da0c
Mattéo Rodrigues <mrodrigues7@myges.fr>
2023-04-11 23:19:56 +0200

    add error page and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import './App.css'
 import React from 'react';
 import HomePage from './HomePage';
+import Card from './Card';
+import ErrorPage from './ErrorPage';
+import MyErrorBoundary from './MyErrorBoundary';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+  },
+  {
+    path: "/garage",
+    element: <Card />,
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
       <div className='background'/>
-      <HomePage />
+      <MyErrorBoundary>
+        <RouterProvider router={router}/>
+      </MyErrorBoundary>
     </div>
   );
 }
---
+++ src/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './Navbar.jsx';
+import demo_gif from './images/course.gif'
+import './HomePage.css';
+
+const Card = () => {
+  return (
+    <>
+     <Navbar />
+    <div className="container mt-5 home-content">
+      <div className="jumbotron">
+        <h1 className="display-4">Bienvenue sur F-Race</h1>
+        <p className="lead">
+          Le pire site pour échanger vos crypto-monnaies rapidement et en toute sécurité.
+        </p>
+        <hr className="my-4" />
+        <p>
+          Inscrivez-vous dès maintenant et commencez à échanger vos crypto-monnaies.
+        </p>
+        <img className='gif-demo' src={demo_gif}/>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Card;
