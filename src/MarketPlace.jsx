48c437224d00c151ba74909af8db0eb23e229dcf
Nairod36 <moulin.solal@gmail.com>
2023-04-13 22:57:22 +0200

    bug garage FIX

--- src/MarketPlace.jsx
+++ src/MarketPlace.jsx
@@ -7,20 +7,20 @@ const MarketPlace = () => {
       <h1 className="text-center mb-4">Marketplace NFT</h1>
       <div className="row">
         <div className="col-md-4">
-          <div className="card">
-            <div className="card-content">
-              <div className="card-title">
+          <div className="cardmp">
+            <div className="card-contentmp">
+              <div className="card-titlemp">
                 <h3>Nom du NFT</h3>
               </div>
-              <div className="img">
-                <img src="lien_vers_image_nft" alt="Nom du NFT" />
+              <div className="imgmp">
+                <img src="lien_vers_image_nft" alt="img" />
               </div>
-              <div className="card-body">
+              <div className="card-bodymp">
                 <p>Description du NFT</p>
                 <p>Prix : 0.5 ETH</p>
                </div>   
-               <div className="card-footer">
-                <a href="#" className="btn btn-primary">Acheter</a>
+               <div className="card-footermp">
+                <a href="#" className="btnmp btn-primary">Acheter</a>
               </div>
             </div>
           </div>
