fecec768d1f3456cc9a55d6a3773933c4724ee33
Nairod36 <moulin.solal@gmail.com>
2023-07-13 17:50:11 +0200

    ajout page Course

---
+++ frontend/src/Race.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Race = () => {
+    return (
+        <div>
+            <h1>Page de course</h1>
+            <p>Bienvenue sur la page de course. Ici, vous pourrez faire courir vos voitures NFT.</p>
+            {/* Ajoutez votre code ici pour implémenter les fonctionnalités de la course */}
+        </div>
+    );
+};
+
+export default Race;
