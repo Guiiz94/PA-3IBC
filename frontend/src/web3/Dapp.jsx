54af4c57407253b3f570c1b0d4b8f358136c27ba
Nairod36 <moulin.solal@gmail.com>
2023-07-16 00:01:09 +0200

    Update Dapp.jsx
    récupération avec id

--- frontend/src/web3/Dapp.jsx
+++ frontend/src/web3/Dapp.jsx
@@ -6,6 +6,7 @@ import TextField from "@material-ui/core/TextField";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Box from "@material-ui/core/Box";
 import "./Dapp.css";
+import { Car } from "../dto"
 import AuctionComponent from "../Encheres.jsx";
 
 // We'll use ethers to interact with the Ethereum network and our contract
@@ -741,7 +742,7 @@ class Dapp extends React.Component {
 
   async enterRace(tokenId) {
     try {
-      const tx = await this._race.enterRace(tokenId);
+      const tx = await this._race.enterRace(tokenId, Car.attributes[0].tokenId, Car.attributes[1].tokenId, Car.attributes[2].tokenId);
       await tx.wait();
     } catch (error) {
       console.error("An error occurred while entering the race: ", error);
