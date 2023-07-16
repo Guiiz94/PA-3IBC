107235677e35d48d63bd9a4de6b513ac16e1ebcf
Guiiz94 <92375190+Guiiz94@users.noreply.github.com>
2023-07-17 01:25:35 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React from 'react';
+import React, { useState } from 'react';
 import HomePage from './HomePage';
 import ErrorPage from './ErrorPage';
 import Garage from './Garage';
@@ -8,40 +8,51 @@ import Navbar from './Navbar';
 import MarketPlace from './MarketPlace';
 import Footer from './footer';
 import Race from './Race';
+import SignUp from './SignUp';
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/home",
-    element: <HomePage />,
-  },
-  {
-    path: "/garage",
-    element: <Garage />,
-  },
-  {
-    path: "/marketplace",
-    element: <MarketPlace />,
-  },
-  {
-    path: "/race",
-    element: <Race />,
-  },
-  {
-    path: "*",
-    element: <ErrorPage />,
-  },
-]);
+import UserList from './UserList';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/home",
+      element: <HomePage />,
+    },
+    {
+      path: "/garage",
+      element: <Garage />,
+    },
+    {
+      path: "/marketplace",
+      element: <MarketPlace />,
+    },
+    {
+      path: "/race",
+      element: <ErrorPage />,
+    },
+    {
+      path: "/test",
+      element: <UserList />,
+    },
+    {
+      path: "/inscription",
+      element: <SignUp />,
+    },
+    {
+      path: "*",
+      element: <ErrorPage />,
+    },
+  ]);
+
   return (
     <div className="App">
       <div className='background'/>
